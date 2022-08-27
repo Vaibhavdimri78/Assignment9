a90b90832226b60ff08e98fa80fd766668112b4d
Vaibhavdimri78 <111555559+Vaibhavdimri78@users.noreply.github.com>
2022-08-27 04:11:52 -0700

    Add files via upload

---
+++ Assignment9.py
@@ -0,0 +1,8 @@
+n=int(input("enter how many times you want to print MySirG on screen"))
+i=1
+while(i<=n):
+    #1 print("MySirG",end='_')
+    #2print(i,end='') 
+    #3 print(i**2,end=' ')
+    print(i*10,end=' ')
+    i+=1
